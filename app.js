400d07ec45187b543c408f41128abc36be157854
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2025-01-29 21:49:40 -0300

    Alterando o valor de 100 para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -7,12 +7,12 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
-}
+};
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
-}
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+};
 
 exibirMensagemInicial();
 
@@ -33,8 +33,8 @@ function verificarChute() {
         }
         tentativas++;
         limparCampo();
-    }
-}
+    };
+};
 
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
@@ -42,28 +42,28 @@ function gerarNumeroAleatorio() {
 
     if (quantidadeDeElementosNaLista == numeroLimite) {
         listaDeNumerosSorteados = [];
-    }
+    };
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
         console.log(listaDeNumerosSorteados)
         return numeroEscolhido;
-    }
-}
+    };
+};
 
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
-}
+};
 
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
-}
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+};
 
 
 
